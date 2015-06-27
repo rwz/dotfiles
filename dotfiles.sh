8d446271cfbbb68329b08142c5759af4706d9bf2
Pavel Pravosud <pavel@pravosud.com>
2015-06-26 23:18:13 -0700

    initial commit

---
+++ dotfiles.sh
@@ -0,0 +1,4 @@
+export DOTFILES_ROOT=$(cd "$( dirname "$0" )" && pwd)
+
+source $DOTFILES_ROOT/zsh/init.sh
+source $DOTFILES_ROOT/vim/init.sh
---
+++ zsh/init.sh
@@ -0,0 +1,4 @@
+autoload -U colors && colors
+setopt promptsubst
+source $DOTFILES_ROOT/zsh/git.zsh
+source $DOTFILES_ROOT/zsh/theme.zsh
