6e0d58c97b8052ddbe850401d9eb7fce192b3308
Pavel Pravosud <pavel@pravosud.com>
2015-07-06 23:59:00 -0700

    Add cloud info to prompt

--- zsh/heroku.sh
+++ zsh/heroku.sh
@@ -2,3 +2,16 @@ export PATH="/usr/local/heroku/bin:$PATH"
 
 alias h="heroku"
 alias ic="ion-client"
+
+export ION_USER=ppravosud
+
+cloud() {
+  eval "$(ion-client shell)"
+  cloud "$@"
+}
+
+heroku_cloud_display() {
+  if [[ ${HEROKU_CLOUD} != 'production' && ${HEROKU_CLOUD} != '' ]]; then
+    echo "%{$fg[blue]%}[%{$fg[cyan]%}${HEROKU_CLOUD:u}%{$fg[blue]%}] "
+  fi
+}
--- zsh/init.sh
+++ zsh/init.sh
@@ -1,7 +1,7 @@
 source $DOTFILES_ROOT/zsh/rvm.sh
 source $DOTFILES_ROOT/zsh/git.sh
-source $DOTFILES_ROOT/zsh/theme.sh
 source $DOTFILES_ROOT/zsh/heroku.sh
+source $DOTFILES_ROOT/zsh/theme.sh
 
 setopt appendhistory
 setopt autocd
