c30d017d3c93ea93aaf18633c633cc8acd0cc09c
Pavel Pravosud <pavel@pravosud.com>
2017-02-03 11:11:34 -0800

    Remove ic and midgard commands

--- fish/functions/heroku_midgard.fish
+++
@@ -1,7 +0,0 @@
-function heroku_midgard
-  set -xg HEROKU_HOST "https://midgard.heroku.com"
-  set -xg HEROKU_SSL_VERIFY "disable"
-  eval $argv
-  set -e HEROKU_HOST
-  set -e HEROKU_SSL_VERIFY
-end
--- fish/heroku.fish
+++ fish/heroku.fish
@@ -1,8 +1,5 @@
 set -gx PATH /usr/local/heroku/bin $PATH
-set -x ION_USER ppravosud
 alias h "heroku"
 alias hs "heroku _sudo --reason reasons"
-alias ic "ion-client"
 alias cloud "heroku_cloud"
 alias debug "heroku_debug"
-alias midgard "heroku_midgard"
