ec35d9ad10aeb90722eeb18756885395d0bf404d
Pavel Pravosud <pavel@pravosud.com>
2017-07-10 10:48:23 -0700

    Always call initial tmux session default

--- tmux/init.fish
+++ tmux/init.fish
@@ -1,2 +1,2 @@
 alias tmux "tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
-alias mux "tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session -A -s (basename (pwd))"
+alias mux "tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session -A -s default"
